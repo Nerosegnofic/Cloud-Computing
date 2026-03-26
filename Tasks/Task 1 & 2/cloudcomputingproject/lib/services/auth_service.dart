65034bcba7d4fd772183337d67528f87add503c3
Ahmed Abdelnabi <77232129+Nerosegnofic@users.noreply.github.com>
2026-03-26 11:24:47 +0200

    Update auth_service.dart

--- Tasks/Task 1 & 2/cloudcomputingproject/lib/services/auth_service.dart
+++ Tasks/Task 1 & 2/cloudcomputingproject/lib/services/auth_service.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
+  static const _key = 'currentUsername';
   static String? currentUsername;
 
   static String normalizeUsername(String username) {
@@ -9,6 +11,9 @@ class AuthService {
 
   static Future<void> login(String username) async {
     currentUsername = normalizeUsername(username);
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_key, currentUsername!);
 
     await FirebaseMessaging.instance.requestPermission();
     await FirebaseMessaging.instance.subscribeToTopic(currentUsername!);
@@ -19,6 +24,14 @@ class AuthService {
       await FirebaseMessaging.instance.unsubscribeFromTopic(currentUsername!);
     }
 
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_key);
+
     currentUsername = null;
+  }
+
+  static Future<String?> getPersistedUsername() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_key);
   }
 }
